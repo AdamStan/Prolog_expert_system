0543af1807516bdf4a67f62e5ebf492db003cfa4
AdamStan <adam15stan@gmail.com>
2018-12-30 23:42:35 +0100

    trzeba zmienic kejsy, propozycje pytan dodalem

--- wybor_kraju_do_emigracji.pl
+++ wybor_kraju_do_emigracji.pl
@@ -18,8 +18,8 @@ czy(X) :- atom_concat('Czy ', X, Y), format(Y).
 klimat_cieply :- czy('klimat ma być cieply?').
 klimat_umiarkowany :- czy("klimat ma być umiarkowany").
 klimat_zimny :- czy("klimat ma być zimny?").
-ksztalt_terenu_gorzysty :- czy("kraj ma być górzysty?").
-ksztalt_terenu_nizinny :- czy("kraj ma być nizinny?").
+/*ksztalt_terenu_gorzysty :- czy("kraj ma być górzysty?").
+ksztalt_terenu_nizinny :- czy("kraj ma być nizinny?").*/
 typ_rozwoju_rozwiniety :- czy("ma byc to kraj rozwinięty?").
 typ_rozwoju_rozwijajacy_sie :- czy("ma to byc kraj rozwijajacy się?").
 typ_rozwoju_nierozwiniety :- czy("ma być to kraj nierozwinięty?").
@@ -27,8 +27,8 @@ kultura_anglosaska :- czy("ma to być kraj anlosaski?").
 kultura_germanska :- czy("czy kraj ma być germański?").
 kultura_romanska :- czy("ma to być kraj romański?").
 kultura_slowianska :- czy("ma to być kraj słowiański?").
-gestosc_zaludnienia_wysoka :- czy("gestość zaludnienia ma być wysoka?").
-gestosc_zaludnienia_niska :- czy("gestość zaludnienia ma być niska?").
+/*gestosc_zaludnienia_wysoka :- czy("gestość zaludnienia ma być wysoka?").
+gestosc_zaludnienia_niska :- czy("gestość zaludnienia ma być niska?").*/
 religia_katolicyzm :- czy("kraj ma być katolicki?").
 religia_protestantyzm :- czy("kraj ma być protestancki?").
 religia_prawoslawie :- czy("kraj ma byc prawosławny?").
@@ -37,6 +37,33 @@ religia_brak :- czy("kraj ma być laicki?").
 blisko_polski :- czy("kraj ma być blisko Polski?").
 socjalny :- czy("kraj ma być socjalny?").
 dostep_do_morza :- czy("kraj ma byc z dostepem do morza?").
+opieka_zdrowotna :- czy("interesuje cie dobra opieka zdrowotna?").
+dlugie_zycie :- czy("chcesz miec wieksze szanse?").
+miasto :- czy("wolisz zycie w miescie niz na wsi?").
+
+lista_pytan([
+	klimat_cieply, 					/*1*/
+	klimat_umiarkowany,				/*2*/
+	klimat_zimny,					/*3*/
+	typ_rozwoju_rozwiniety,			/*4*/
+	typ_rozwoju_rozwijajacy_sie,	/*5*/
+	typ_rozwoju_nierozwiniety,		/*6*/
+	kultura_anglosaska,				/*7*/
+	kultura_germanska,				/*8*/
+	kultura_romanska,				/*9*/
+	kultura_slowianska,				/*10*/
+	religia_katolicyzm,				/*11*/
+	religia_protestantyzm,			/*12*/
+	religia_prawoslawie,			/*13*/
+	religia_islam,					/*14*/
+	religia_brak,					/*15*/
+	blisko_polski,					/*16*/
+	socjalny,						/*17*/
+	dostep_do_morza,				/*18*/
+	opieka_zdrowotna,				/*19*/
+	dlugie_zycie,					/*20*/
+	miasto							/*21*/
+]).
 
 /* baza wiedzy */
 albania :- 	case(klimat_cieply), 
@@ -299,13 +326,48 @@ wlochy :- 	case(klimat_cieply),
 			case(gestosc_zaludnienia_wysoka),
 			case(religia_katolicyzm),
 			case(socjalny).
+
+lista_odpowiedzi([
+	wlochy, wielka_brytania, wegry, watykan, ukraina,
+	turcja, szwecja, szwajcaria, slowenia, slowacja, serbia,
+	san_marino, rumunia, rosja, portugalia, norwegia, niemcy,
+	monako, moldawia, malta, macedonia, lotwa, luksemburg,
+	litwa, liechtenstein, kazachstan, islandia, irlandia, holandia,
+	hiszpania, grecja, francja, finlandia, estonia, dania, 
+	czechy, czarnogora, chorwacja, belgia, bialorus, bosnia_i_hercegowina,
+	bulgaria, austria, albania
+]);
 			
-pytanieCzy(Question) :-
-        Question,
-        read(Response),
-        ( (Response == yes ; Response == y)
-                ->
-                asserta(case(Question)) ;
-                _, fail).
-				
-start_p :- \+(albania,wlochy).
+pierwszy([H|T], H, T). 
+
+ankieta(X) :- 
+	pierwszy(X, Question, Tail), 
+	Question, 
+	read(Response), 
+	( (Response == yes ; Response == y) -> asserta(case(Question)) ; true ), 
+	list_empty(Tail, Ifempty),
+	( not(Ifempty) -> 
+		ankieta(Tail) ;
+		lista_odpowiedzi(Z), pseudownioskowanie(Z) 
+	).
+
+/* Zamiast write'a to damy pseudownioskowanie, jak lista bedzie pusta*/
+start :- lista_pytan(X), ankieta(X).
+
+pseudownioskowanie(X) :-
+	pierwszy(X, Wniosek, Ogon),
+	write(Wniosek),
+	write("\n"),
+	( not(Wniosek) ->
+		write(" -> False \n") ;
+		write(" -> True \n"), assert(conclusion(Wniosek)) 
+	),
+	list_empty(Ogon, Ifempty),
+	( not(Ifempty) -> 
+		pseudownioskowanie(Ogon) ;
+		czysczenie
+	).
+
+czysczenie :-
+	retractall(conclusion(_)),
+	retractall(case(_)).
