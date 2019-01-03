bb8277761bc28505134d21926affd123dfd84322
AdamStan <adam15stan@gmail.com>
2019-01-03 21:07:36 +0100

    zobaczymy czy wersja ostatenczna

--- wybor_kraju_do_emigracji.pl
+++ wybor_kraju_do_emigracji.pl
@@ -1,5 +1,6 @@
 /* wybor_kraju_do_emigracji */
 :- dynamic case/1.
+:- dynamic conclusion/1.
 
 /*
 cechy:
@@ -15,28 +16,28 @@ cechy:
 */
 czy(X) :- atom_concat('Czy ', X, Y), format(Y).
 /* Pytania */
-klimat_cieply :- czy('klimat ma być cieply?').
-klimat_umiarkowany :- czy("klimat ma być umiarkowany").
-klimat_zimny :- czy("klimat ma być zimny?").
-/*ksztalt_terenu_gorzysty :- czy("kraj ma być górzysty?").
-ksztalt_terenu_nizinny :- czy("kraj ma być nizinny?").*/
-typ_rozwoju_rozwiniety :- czy("ma byc to kraj rozwinięty?").
-typ_rozwoju_rozwijajacy_sie :- czy("ma to byc kraj rozwijajacy się?").
-typ_rozwoju_nierozwiniety :- czy("ma być to kraj nierozwinięty?").
-kultura_anglosaska :- czy("ma to być kraj anlosaski?").
-kultura_germanska :- czy("czy kraj ma być germański?").
-/*kultura_romanska :- czy("ma to być kraj romański?").*/
-kultura_slowianska :- czy("ma to być kraj słowiański?").
-/*gestosc_zaludnienia_wysoka :- czy("gestość zaludnienia ma być wysoka?").
-gestosc_zaludnienia_niska :- czy("gestość zaludnienia ma być niska?").*/
-religia_katolicyzm :- czy("kraj ma być katolicki?").
-religia_protestantyzm :- czy("kraj ma być protestancki?").
-religia_prawoslawie :- czy("kraj ma byc prawosławny?").
-religia_islam :- czy("kraj ma być mułzumański?").
-religia_brak :- czy("kraj ma być laicki?").
-/*blisko_polski :- czy("kraj ma być blisko Polski?").*/
-socjalny :- czy("kraj ma być socjalny?").
-dostep_do_morza :- czy("kraj ma byc z dostepem do morza?").
+klimat_cieply :- czy('klimat moze byc cieply?').
+klimat_umiarkowany :- czy("klimat moze być umiarkowany").
+klimat_zimny :- czy("klimat może byc zimny?").
+/*ksztalt_terenu_gorzysty :- czy("kraj może być górzysty?").
+ksztalt_terenu_nizinny :- czy("kraj może być nizinny?").*/
+typ_rozwoju_rozwiniety :- czy("moze byc to kraj rozwiniety?").
+typ_rozwoju_rozwijajacy_sie :- czy("moze to byc kraj rozwijajacy sie?").
+typ_rozwoju_nierozwiniety :- czy("moze być to kraj nierozwiniety?").
+kultura_anglosaska :- czy("moze to byc kraj anlosaski?").
+kultura_germanska :- czy("czy kraj moze byc germanski?").
+/*kultura_romanska :- czy("może to być kraj romański?").*/
+kultura_slowianska :- czy("moze to byc kraj slowianski?").
+/*gestosc_zaludnienia_wysoka :- czy("gestość zaludnienia może być wysoka?").
+gestosc_zaludnienia_niska :- czy("gestość zaludnienia może być niska?").*/
+religia_katolicyzm :- czy("kraj moze byc katolicki?").
+religia_protestantyzm :- czy("kraj moze byc protestancki?").
+religia_prawoslawie :- czy("kraj moze byc prawoslawny?").
+religia_islam :- czy("kraj moze byc mulzumanski?").
+religia_brak :- czy("kraj moze byc laicki?").
+/*blisko_polski :- czy("kraj może być blisko Polski?").*/
+socjalny :- czy("kraj moze byc socjalny?").
+dostep_do_morza :- czy("kraj moze byc z dostepem do morza?").
 /*opieka_zdrowotna :- czy("interesuje cie dobra opieka zdrowotna?").*/
 /*dlugie_zycie :- czy("chcesz miec wieksze szanse?").*/
 /*miasto :- czy("wolisz zycie w miescie niz na wsi?").*/
@@ -57,7 +58,7 @@ lista_pytan([
 	religia_islam,					/*13*/
 	religia_brak,					/*14*/
 	socjalny,					/*15*/	
-	dostep_do_morza,				/*16*/
+	dostep_do_morza				/*16*/
 ]).
 
 /* baza wiedzy */
@@ -101,12 +102,12 @@ chorwacja :-	case(klimat_cieply),
 		case(typ_rozwoju_rozwijajacy_sie),
 		case(kultura_slowianska),
 		case(religia_katolicyzm),
-		case(dostep_do_morza),
+		case(dostep_do_morza).
 			
 czarnogora :-	case(klimat_cieply), 
 		case(typ_rozwoju_rozwijajacy_sie),
 		case(kultura_slowianska),
-		case(religia_katolicyzm).
+		case(religia_katolicyzm),
 		case(dostep_do_morza).
 			
 czechy :-	case(klimat_umiarkowany),
@@ -228,8 +229,8 @@ portugalia :-	case(klimat_cieply),
 rosja :-	case(klimat_zimny),
 		case(typ_rozwoju_rozwijajacy_sie),
 		case(kultura_slowianska),
-		case(religia_prawoslawie).
-		case(dostep_do_morza),
+		case(religia_prawoslawie),
+		case(dostep_do_morza).
 			
 rumunia :-	case(klimat_umiarkowany),
 		case(typ_rozwoju_rozwijajacy_sie),	
@@ -297,15 +298,21 @@ wlochy :- 	case(klimat_cieply),
 		case(dostep_do_morza).
 
 lista_odpowiedzi([
-	wlochy, wielka_brytania, wegry, watykan, ukraina,
+	niemcy, wielka_brytania, rosja, wlochy, wegry, ukraina,
 	turcja, szwecja, szwajcaria, slowenia, slowacja, serbia,
-	san_marino, rumunia, rosja, portugalia, norwegia, niemcy,
+	san_marino, rumunia, portugalia, norwegia, watykan,
 	monako, moldawia, malta, macedonia, lotwa, luksemburg,
-	litwa, liechtenstein, kazachstan, islandia, irlandia, holandia,
+	litwa, liechtenstein, islandia, irlandia, holandia,
 	hiszpania, grecja, francja, finlandia, estonia, dania, 
 	czechy, czarnogora, chorwacja, belgia, bialorus, bosnia_i_hercegowina,
 	bulgaria, austria, albania
-]);
+]).
+
+list_empty(List, Empty) :-
+    length(List, Len),
+    (   Len == 0
+    ->  Empty = true
+    ;   Empty = false).
 			
 pierwszy([H|T], H, T). 
 
@@ -313,7 +320,7 @@ ankieta(X) :-
 	pierwszy(X, Question, Tail), 
 	Question, 
 	read(Response), 
-	( (Response == yes ; Response == y) -> asserta(case(Question)) ; true ), 
+	( (Response == yes ; Response == y) -> assertz(case(Question)) ; true ), 
 	list_empty(Tail, Ifempty),
 	( not(Ifempty) -> 
 		ankieta(Tail) ;
@@ -325,18 +332,18 @@ start :- lista_pytan(X), ankieta(X).
 
 pseudownioskowanie(X) :-
 	pierwszy(X, Wniosek, Ogon),
-	write(Wniosek),
-	write("\n"),
 	( not(Wniosek) ->
-		write(" -> False \n") ;
-		write(" -> True \n"), assert(conclusion(Wniosek)) 
+		true ;
+		write(Wniosek), write(" -> True \n"), assertz(conclusion(Wniosek)) 
 	),
 	list_empty(Ogon, Ifempty),
 	( not(Ifempty) -> 
 		pseudownioskowanie(Ogon) ;
-		czysczenie
-	).
+		conclusion(Panstwo) ->
+			write("Pierwszy 'wywnioskowany' kraj: "), write(Panstwo) ;
+			write("W bazie nie ma panstwa spelniajacego podane warunki")
+	), czyszczenie.
 
-czysczenie :-
+czyszczenie :-
 	retractall(conclusion(_)),
 	retractall(case(_)).
